6f7851af43fd5edf8a73c3aa7d3b416b71f23a86
sevaphasol <93792724+sevaphasol@users.noreply.github.com>
2022-02-01 17:27:28 +0300

    game background

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+import pygame.font
+
 from default_commands import *
 
 
 FPS = 50
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 915
+HEIGHT = 1005
 
 
 class Game:
@@ -29,9 +31,12 @@ class Game:
             self.clock.tick(FPS)
         terminate()
 
-    def start_screen(self):
+    def clear_screen(self):
         self.screen.fill((0, 0, 0))
         pygame.display.flip()
+
+    def start_screen(self):
+        self.clear_screen()
 
         all_sprites = pygame.sprite.Group()
 
@@ -40,7 +45,7 @@ class Game:
         pic_sprite.image = pygame.transform.scale(load_image("pic1.jpg"), (pic_sprite_width,
                                                                            pic_sprite_height))
         pic_sprite.rect = pic_sprite.image.get_rect()
-        pic_sprite.rect.x = 270
+        pic_sprite.rect.x = 230
         pic_sprite.rect.y = 60
 
         heading_sprite = pygame.sprite.Sprite(all_sprites)
@@ -48,25 +53,25 @@ class Game:
         heading_sprite.image = pygame.transform.scale(load_image("heading.png"), (heading_sprite_width,
                                                                                   heading_sprite_height))
         heading_sprite.rect = heading_sprite.image.get_rect()
-        heading_sprite.rect.x = 170
+        heading_sprite.rect.x = 130
         heading_sprite.rect.y = 330
 
         start_sprite = pygame.sprite.Sprite(all_sprites)
         start_sprite.image = load_image("start.png")
         start_sprite.rect = start_sprite.image.get_rect()
-        start_sprite.rect.x = 410
+        start_sprite.rect.x = 370
         start_sprite.rect.y = 530
 
         controls_sprite = pygame.sprite.Sprite(all_sprites)
         controls_sprite.image = load_image("controls.png")
         controls_sprite.rect = controls_sprite.image.get_rect()
-        controls_sprite.rect.x = 360
+        controls_sprite.rect.x = 320
         controls_sprite.rect.y = 590
 
         credit_sprite = pygame.sprite.Sprite(all_sprites)
         credit_sprite.image = load_image("credit.png")
         credit_sprite.rect = credit_sprite.image.get_rect()
-        credit_sprite.rect.x = 400
+        credit_sprite.rect.x = 360
         credit_sprite.rect.y = 650
 
         all_sprites.draw(self.screen)
@@ -76,8 +81,7 @@ class Game:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     terminate()
-                elif event.type == pygame.KEYDOWN or \
-                        event.type == pygame.MOUSEBUTTONDOWN:
+                elif event.type == pygame.MOUSEBUTTONDOWN:
                     cords = event.pos
                     if is_in(cords, start_sprite.rect):
                         print('start')
@@ -91,9 +95,23 @@ class Game:
                         print('credit')
                         self.state = "credit"
                         return
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
+                        print('start')
+                        self.state = "game"
+                        return
 
     def game_screen(self):
-        self.screen.fill((0, 0, 0))
+        self.clear_screen()
+
+        fon = pygame.transform.scale(load_image("background.png"), (WIDTH - 100, HEIGHT - 100))
+        self.screen.blit(fon, (50, 80))
+
+        font = pygame.font.Font(None, 60)
+        self.screen.blit(font.render("HIGH SCORE", True, pygame.Color('white')), (60, 25))
+        self.screen.blit(font.render("0", True, pygame.Color('white')), (446, 25))
+        self.screen.blit(font.render("0", True, pygame.Color('white')), (680, 25))
+
         pygame.display.flip()
 
         while True:
@@ -102,8 +120,7 @@ class Game:
                     terminate()
 
     def controls_screen(self):
-        self.screen.fill((0, 0, 0))
-        pygame.display.flip()
+        self.clear_screen()
 
         while True:
             for event in pygame.event.get():
@@ -111,8 +128,7 @@ class Game:
                     terminate()
 
     def credit_screen(self):
-        self.screen.fill((0, 0, 0))
-        pygame.display.flip()
+        self.clear_screen()
 
         while True:
             for event in pygame.event.get():
