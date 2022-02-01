13829f4ef43d1c422ad6734e474d49b14caebf08
sevaphasol <93792724+sevaphasol@users.noreply.github.com>
2022-02-01 16:11:58 +0300

    ООП

--- main.py
+++ main.py
@@ -1,49 +1,127 @@
-import os
-import sys
-import pygame
-
-pygame.init()
-size = width, height = 1000, 1000
-screen = pygame.display.set_mode(size)
+from default_commands import *
 
 
-def load_image(name, color_key=None):
-    fullname = os.path.join('data', name)
-    if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найден")
-        sys.exit()
-    image = pygame.image.load(fullname)
-    if color_key is not None:
-        image = image.convert()
-        if color_key == -1:
-            color_key = image.get_at((0, 0))
-        image.set_colorkey(color_key)
-    else:
-        image = image.convert_alpha()
-    return image
+FPS = 50
+WIDTH = 1000
+HEIGHT = 1000
 
 
-all_sprites = pygame.sprite.Group()
-sprite = pygame.sprite.Sprite(all_sprites)
-sprite.image = load_image("creature.png")
-sprite.rect = sprite.image.get_rect()
-all_sprites.add(sprite)
-running = True
-pygame.mouse.set_visible(False)
-while running:
-    screen.fill((255, 255, 255))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                sprite.rect.x += 10
-            elif event.key == pygame.K_LEFT:
-                sprite.rect.x -= 10
-            elif event.key == pygame.K_DOWN:
-                sprite.rect.y += 10
-            elif event.key == pygame.K_UP:
-                sprite.rect.y -= 10
-    all_sprites.draw(screen)
-    pygame.display.flip()
-pygame.quit()
+class Game:
+    def __init__(self):
+        pygame.init()
+        pygame.display.set_caption("PACMAN")
+        pygame.display.set_icon(pygame.image.load("data/icon.png"))
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.state = "start"
+
+    def run(self):
+        while self.running:
+            if self.state == "start":
+                self.start_screen()
+            elif self.state == "game":
+                self.game_screen()
+            elif self.state == "controls":
+                self.controls_screen()
+            elif self.state == "credit":
+                self.credit_screen()
+            self.clock.tick(FPS)
+        terminate()
+
+    def start_screen(self):
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        all_sprites = pygame.sprite.Group()
+
+        pic_sprite = pygame.sprite.Sprite(all_sprites)
+        pic_sprite_width, pic_sprite_height = [i * 0.8 for i in load_image("pic1.jpg").get_size()]
+        pic_sprite.image = pygame.transform.scale(load_image("pic1.jpg"), (pic_sprite_width,
+                                                                           pic_sprite_height))
+        pic_sprite.rect = pic_sprite.image.get_rect()
+        pic_sprite.rect.x = 270
+        pic_sprite.rect.y = 60
+
+        heading_sprite = pygame.sprite.Sprite(all_sprites)
+        heading_sprite_width, heading_sprite_height = [i * 0.8 for i in load_image("heading.png").get_size()]
+        heading_sprite.image = pygame.transform.scale(load_image("heading.png"), (heading_sprite_width,
+                                                                                  heading_sprite_height))
+        heading_sprite.rect = heading_sprite.image.get_rect()
+        heading_sprite.rect.x = 170
+        heading_sprite.rect.y = 330
+
+        start_sprite = pygame.sprite.Sprite(all_sprites)
+        start_sprite.image = load_image("start.png")
+        start_sprite.rect = start_sprite.image.get_rect()
+        start_sprite.rect.x = 410
+        start_sprite.rect.y = 530
+
+        controls_sprite = pygame.sprite.Sprite(all_sprites)
+        controls_sprite.image = load_image("controls.png")
+        controls_sprite.rect = controls_sprite.image.get_rect()
+        controls_sprite.rect.x = 360
+        controls_sprite.rect.y = 590
+
+        credit_sprite = pygame.sprite.Sprite(all_sprites)
+        credit_sprite.image = load_image("credit.png")
+        credit_sprite.rect = credit_sprite.image.get_rect()
+        credit_sprite.rect.x = 400
+        credit_sprite.rect.y = 650
+
+        all_sprites.draw(self.screen)
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+                elif event.type == pygame.KEYDOWN or \
+                        event.type == pygame.MOUSEBUTTONDOWN:
+                    cords = event.pos
+                    if is_in(cords, start_sprite.rect):
+                        print('start')
+                        self.state = "game"
+                        return
+                    if is_in(cords, controls_sprite.rect):
+                        print('controls')
+                        self.state = "controls"
+                        return
+                    if is_in(cords, credit_sprite.rect):
+                        print('credit')
+                        self.state = "credit"
+                        return
+
+    def game_screen(self):
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+
+    def controls_screen(self):
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+
+    def credit_screen(self):
+        self.screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    terminate()
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
+    pygame.quit()
+    sys.exit()
